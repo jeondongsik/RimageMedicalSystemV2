d5947b5f550b72747f3a765048ea0e2117712d57
jeondongsik <46669277+jeondongsik@users.noreply.github.com>
2022-02-01 23:50:40 +0900

    어셈블리 버전 수정

--- RimageMedicalSystemV2/Properties/AssemblyInfo.cs
+++ RimageMedicalSystemV2/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RimageMedicalSystemV23")]
+[assembly: AssemblyTitle("RimageMedicalSystemV2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RimageMedicalSystemV23")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("RimageMedicalSystemV2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.20.1.8")]
-[assembly: AssemblyFileVersion("1.20.1.8")]
+[assembly: AssemblyVersion("1.22.2.1")]
+[assembly: AssemblyFileVersion("1.22.2.1")]
